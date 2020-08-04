87a6b38d88fd1fdbfad61fcc52e9b68057c8406e
wyodoodoyw <mwood051@gmail.com>
2020-08-04 15:42:35 -0400

    populate cells in table

--- InspoQuotes/Controllers/QuoteTableViewController.swift
+++ InspoQuotes/Controllers/QuoteTableViewController.swift
@@ -31,34 +31,20 @@ class QuoteTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return quotesToShow.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+        cell.textLabel?.text = quotesToShow[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -105,9 +91,7 @@ class QuoteTableViewController: UITableViewController {
     }
     */
     
-    
-    
-    
+    // MARK: - IBActions
     
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
         
